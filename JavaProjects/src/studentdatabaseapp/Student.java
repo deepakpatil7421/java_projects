2b5c510d42975d013baafa4d42addc9154ee90f9
deepakpatil7421 <deepakpatil7421@gmail.com>
2018-06-14 23:16:21 +0530

    new added app

---
+++ JavaProjects/src/studentdatabaseapp/Student.java
@@ -0,0 +1,54 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int gradeYear;
+	private String courses = null;
+	private int tuitionBalance = 0;
+	private String StudentID;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
+
+	public Student() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the student first name");
+		this.firstName = sc.nextLine();
+
+		System.out.println("Enter the last name");
+		this.lastName = sc.nextLine();
+
+		System.out.println("1- Freshmen \n2 - Sophomore \n3 - Junior \n4 - Senior \nEnter the Student Class Level");
+		this.gradeYear = sc.nextInt();
+		setStudentID();
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + StudentID);
+
+	}
+
+	private void setStudentID() {
+		id++;
+		this.StudentID = gradeYear + "" + id;
+	}
+
+	public void enroll() {
+		do {
+
+			System.out.print("Enter the course to enroll (Q to Quit)");
+			Scanner sc = new Scanner(System.in);
+			String course = sc.nextLine();
+			if (!course.equals("Q")) {
+				courses = courses + "\n" + course;
+				tuitionBalance = tuitionBalance + costOfCourse;
+
+			} else {
+				break;
+			}
+
+		} while (1 != 0);
+		System.out.println("ENROLLED IN :" + courses);
+		System.out.println("Tuition Balance :" + tuitionBalance);
+	}
+
+}
---
+++ JavaProjects/src/studentdatabaseapp/StudentDataBaseApplication.java
@@ -0,0 +1,12 @@
+package studentdatabaseapp;
+
+public class StudentDataBaseApplication {
+
+	public static void main(String[] args) {
+		Student stu=new Student();
+		stu.enroll();
+		
+
+	}
+
+}
